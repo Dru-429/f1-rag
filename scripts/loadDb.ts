55a38badf56211ecbbfb01ee618dca299960e543
Dru-429 <ect.dhruv@gmail.com>
2025-03-16 23:54:45 +0530

    Added data to scrape

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -16,3 +16,15 @@ const {
 
 const openai = new OpenAI({ apiKey: OPENAI_API_KEY })
 
+const f1Data = [
+    'https://en.wikipedia.org/wiki/Formula_One',
+    'https://www.skysports.com/f1/news/12433/13117256/lewis-hamilton-says-move-to-ferrary',
+    'https://www.formula1.com/en/latest/all',
+    'https://www.forbes.com/sites/brettknight/2023/11/29/formula-1s-highest-paid-driver',
+    'https://en.wikipedia.org/wiki/2023_Formula_One_World_Championship',
+    'https://en.wikipedia.org/wiki/2022_Formula_One_World_Championship',
+    'https://en.wikipedia.org/wiki/List_of_Formula_One_World_Drivers%27_Champions',
+    'https://en.wikipedia.org/wiki/2024_Formula_One_World_Championship',
+    'https://www.formula1.com/en/results.html/2024/races.html',
+    'https://www.formula1.com/en/racing/2024.html'
+  ];
